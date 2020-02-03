300d245cbaf981f87eb8e2ada46bf4eb4d055ae1
nature1995 <gzr2008@vip.qq.com>
2020-02-02 19:53:32 -0500

    init

---
+++ extra_apps/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2/2/20 7:32 PM
+# @Author  : naturegong
+# @license : Copyright(C), Tencent
+# @Contact : naturegong@tencent.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @Version : Python 2.7
