734357361df88745d07a0a3977b78317912c8cd3
nature1995 <gzr2008@vip.qq.com>
2020-02-06 12:48:31 -0500

    add user_operation model

--- VueDjangoShopWebsite/settings.py
+++ VueDjangoShopWebsite/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DjangoUeditor'
+    'DjangoUeditor',
     'users',
     'goods',
     'trade',
